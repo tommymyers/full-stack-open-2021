0951fcdf9dacb6a7eb3047cce3a2bcd02d2db97b
Tommy Myers <tommy@tommymyers.dev>
2021-05-29 20:46:26 +0100

    exercise 1.11

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,12 +1,15 @@
 import React, { useState } from 'react'
 
-const FeedbackButton = ({text, onClick}) => {
+const FeedbackButton = ({ text, onClick }) => {
   return <button onClick={onClick}>{text}</button>
 }
 
 const Statistic = ({ text, value }) => {
   return (
-    <p>{text} {value}</p>
+    <tr>
+      <td>{text}</td>
+      <td>{value}</td>
+    </tr>
   )
 }
 
@@ -14,14 +17,16 @@ const Statistics = ({ good, neutral, bad }) => {
   const total = good + neutral + bad
   if (total > 0) {
     return (
-      <div>
-        <Statistic text="good" value={good} />
-        <Statistic text="neutral" value={neutral} />
-        <Statistic text="bad" value={bad} />
-        <Statistic text="all" value={total} />
-        <Statistic text="average" value={(good - bad) / total} />
-        <Statistic text="positive" value={good / total * 100} />
-      </div>
+      <table>
+        <tbody>
+          <Statistic text="good" value={good} />
+          <Statistic text="neutral" value={neutral} />
+          <Statistic text="bad" value={bad} />
+          <Statistic text="all" value={total} />
+          <Statistic text="average" value={(good - bad) / total} />
+          <Statistic text="positive" value={good / total * 100} />
+        </tbody>
+      </table>
     )
   } else {
     return <p>No feedback given</p>
