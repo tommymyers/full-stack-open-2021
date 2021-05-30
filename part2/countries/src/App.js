37bb7ed0f01a5ea3c88bb846720739e1da02766f
Tommy Myers <tommy@tommymyers.dev>
2021-05-30 21:19:49 +0100

    exercise 2.14

--- part2/countries/src/App.js
+++ part2/countries/src/App.js
@@ -1,6 +1,35 @@
 import React, { useEffect, useState } from 'react'
 import axios from "axios"
 
+const Country = ({ country }) => {
+  const weatherApiKey = process.env.REACT_APP_API_KEY
+  const [weather, setWeather] = useState({})
+  const hook = () => {
+    axios.get(`http://api.weatherstack.com/current?access_key=${weatherApiKey}&query=${country.capital}`)
+      .then(response => setWeather(response.data.current))
+  }
+  useEffect(hook, [])
+
+  const weatherIcons = weather.weather_icons || []
+
+  return (
+    <div>
+      <h2>{country.name}</h2>
+      <div>capital {country.capital}</div>
+      <div>population {country.population}</div>
+      <h3>languages</h3>
+      <ul>
+        {country.languages.map(language => <li key={language.name}>{language.name}</li>)}
+      </ul>
+      <img src={country.flag} width={100} alt={`${country.name} flag`}></img>
+      <h3>Weather in {country.capital}</h3>
+      <div><b>Temperature: </b>{weather.temperature || ""}</div>
+      {weatherIcons.map(icon => <img src={icon} key={icon} alt={icon} />)}
+      <div><b>Wind: </b>{weather.wind_speed || ""} mph direction {weather.wind_dir || ""}</div>
+    </div>
+  )
+}
+
 const App = () => {
   const hook = () => {
     axios.get("https://restcountries.eu/rest/v2/all").then(response => setCountries(response.data))
@@ -17,16 +46,7 @@ const App = () => {
 
   if (filteredCountries.length === 1) {
     const [country] = filteredCountries
-    searchResults = <div>
-      <h2>{country.name}</h2>
-      <div>capital {country.capital}</div>
-      <div>population {country.population}</div>
-      <h3>languages</h3>
-      <ul>
-        {country.languages.map(language => <li key={language.name}>{language.name}</li>)}
-      </ul>
-      <img src={country.flag} width={100} alt={`${country.name} flag`}></img>
-    </div>
+    searchResults = <Country country={country} />
   } else if (filteredCountries.length < 10) {
     searchResults = filteredCountries.map(country =>
       <div key={country.name}>
