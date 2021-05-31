b9573c3c38fa0e9add54cc3e3a6d1f3b75de2545
Tommy Myers <tommy@tommymyers.dev>
2021-05-31 04:26:49 +0100

    exercise 2.20

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,5 +1,11 @@
 import React, { useEffect, useState } from 'react'
 import personsService from "./services/personsService"
+import "./App.css"
+
+const Notification = ({ notification }) => {
+  if (!notification.message) return null
+  return <div className={`notification notification__${notification.type}`}>{notification.message}</div>
+}
 
 const Filter = ({ searchQuery, onSearchQueryChange }) => {
   return <div>Search for: <input value={searchQuery} onChange={onSearchQueryChange} /></div>
@@ -24,16 +30,29 @@ const People = ({ people, onDeletePerson }) => {
 }
 
 const App = () => {
-  const [persons, setPersons] = useState([])
+  const [currentNotification, setCurrentNotification] = useState({})
+  const [searchQuery, setSearchQuery] = useState('')
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
-  const [searchQuery, setSearchQuery] = useState('')
-  const hook = () => personsService.getAll().then((people) => setPersons(people))
+  const [persons, setPersons] = useState([])
+  const hook = () => personsService.getAll().then(people => setPersons(people))
   useEffect(hook, [])
 
   const newNameInputChange = (event) => setNewName(event.target.value)
   const newNumberInputChange = (event) => setNewNumber(event.target.value)
   const searchQueryChange = (event) => setSearchQuery(event.target.value)
+
+  const displayNotification = (notification) => {
+    setCurrentNotification(notification)
+    setTimeout(() => {
+      setCurrentNotification({})
+    }, 3000);
+  };
+
+  const clearFields = () => {
+    setNewName("")
+    setNewNumber("")
+  }
 
   const onFormSubmit = (event) => {
     event.preventDefault()
@@ -44,6 +63,8 @@ const App = () => {
         personsService.update(person.id, { ...person, number: newNumber })
           .then(updatedPerson => {
             setPersons(persons.map(p => p.id !== person.id ? p : updatedPerson))
+            clearFields()
+            displayNotification({ type: "success", message: `Updated ${person.name}` })
           })
       }
     } else {
@@ -56,8 +77,8 @@ const App = () => {
         .then(p => {
           console.log(p)
           setPersons([...persons, p])
-          setNewName("")
-          setNewNumber("")
+          clearFields()
+          displayNotification({ type: "success", message: `Added ${p.name}` })
         })
     }
   }
@@ -65,7 +86,11 @@ const App = () => {
   const onDeletePerson = (person) => {
     if (!window.confirm(`Are you sure you want to delete ${person.name}?`)) return
     personsService.remove(person.id)
+      .catch(() => {
+        displayNotification({ type: "error", message: `${person.name} was already deleted` })
+      })
     setPersons(persons.filter(p => p.id !== person.id))
+    displayNotification({ type: "error", message: `Deleted ${person.name}` })
   }
 
   const filteredPersons = persons.filter(p => p.name.toLowerCase().includes(searchQuery.toLowerCase()))
@@ -73,6 +98,7 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
+      <Notification notification={currentNotification} />
       <Filter searchQuery={searchQuery} onSearchQueryChange={searchQueryChange} />
       <h3>Insert new:</h3>
       <PersonForm
